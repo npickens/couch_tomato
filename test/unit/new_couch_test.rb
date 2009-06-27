4aba98202303d8a94c2f6ca152d62381cb39b4da
Mariano Lizarraga <malife@gmail.com>
2009-06-27 09:09:20 -0700

    Included tests to guarantee the callbacks are called on save and update doc

--- test/unit/new_couch_test.rb
+++ test/unit/new_couch_test.rb
@@ -25,7 +25,7 @@ class TestNewCouch < Test::Unit::TestCase
       assert_respond_to TestDb.couchrest_db, :info
     end #instatiate couchrest
     
-    context "with a correctly configured database -- server" do
+    context "with a correctly configured database and server" do
       setup do
         stub(TestDb).database_name{'hello-world'}
         stub(TestDb).database_server{'http://127.0.0.1:5984/'}
@@ -53,13 +53,13 @@ class TestNewCouch < Test::Unit::TestCase
         assert_equal TestDb.views[:hello][:design_doc].to_s, 'hello-world'
       end #should have data from view
       
-      context "if given a document to save with a certain class" do
+      context "if given a document to save" do
         setup do
           @document = Object.new
           stub(@document).dirty?{1}   
         end
 
-        should "call create_doc if the document is new" do               
+        should "call create_doc if the document is new" do
           stub(@document).new?{1}     
           stub(TestDb).create_document {true}   
           dont_allow(TestDb).update_document(@document)
@@ -73,9 +73,43 @@ class TestNewCouch < Test::Unit::TestCase
           assert_equal TestDb.save_document(@document), true    
         end
         
-      end
-      
-                
+        context ", " do
+          setup do
+            stub(@document).valid?{1}
+            stub(@document).to_hash{1}
+
+            stub(TestDb.couchrest_db).save_doc{{'rev' => '1', 'id' => '123', }}
+            stub(@document)._rev=('1')
+            
+            mock(@document).run_callbacks(:before_validation_on_save)
+            mock(@document).run_callbacks(:before_save)
+            mock(@document).run_callbacks(:after_save)
+          end
+
+          should ", if the document is new, call the required callbacks before saving it" do
+            stub(@document).new?{1}                        
+
+            stub(@document).database=(TestDb)
+            stub(@document)._id=('123') 
+          
+            mock(@document).run_callbacks(:before_validation_on_create)
+            mock(@document).run_callbacks(:before_create)
+            mock(@document).run_callbacks(:after_create)
+          
+            assert_equal TestDb.save_document(@document), true
+          end
+          
+          should ", if the document is not new, call the required callbacks before saving it" do
+            stub(@document).new?{nil}               
+ 
+            mock(@document).run_callbacks(:before_validation_on_update)
+            mock(@document).run_callbacks(:before_update)
+            mock(@document).run_callbacks(:after_update)
+          
+            assert_equal TestDb.save_document(@document), true
+          end          
+        end # context ,
+      end #context if given a doc to save
     end # context with db and serv
   end #context a Db  
 end # class
